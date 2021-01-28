5ecee7e60528a54ebcb54c63648a9661903e5551
soo15 <46892179+soo15@users.noreply.github.com>
2021-01-28 15:45:15 +0900

    apple_js(start)

---
+++ apple-clone-v9/apple-clone-v9/js/main_test.js
@@ -0,0 +1,51 @@
+//즉시함수호출
+(()=>{
+	//전역변수사용을 피하려고..즉시함수사용
+	const sceneInfo = [
+		{
+			//0
+			type : 'sticky',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs:{
+				container: document.querySelector('#scroll-section-0')
+			}
+		},
+		{
+			//1
+			type : 'normal',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs:{
+				container: document.querySelector('#scroll-section-1')
+			}
+		},
+		{
+			//2
+			type : 'sticky',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs:{
+				container: document.querySelector('#scroll-section-2')
+			}
+		},
+		{
+			//3 
+			type : 'sticky',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs:{
+				container: document.querySelector('#scroll-section-3')
+			}
+		}
+	];
+
+	function setLayout(){
+		for(let i=0; i < sceneInfo.length ; i++){
+			sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+			sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+		}
+	}
+	window.addEventListener('resize', setLayout);
+	setLayout();
+})();
