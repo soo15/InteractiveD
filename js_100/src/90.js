76e30462125a16e0b5e74eff1668dac8612dc72a
soo15 <46892179+soo15@users.noreply.github.com>
2021-03-24 16:57:31 +0900

    TEST

--- js_100/src/90.js
+++ js_100/src/90.js
@@ -1,18 +1,25 @@
+
 let l = [];
 
+//A~Z 원소
 for(let i = 65; i < 91; i++){
+    //아스키코드변환해서..PUSH
     l.push(String.fromCharCode(i));
 }
 
+
 function randomItem(a){
     let string = [];
-    while(string.length != 8){
-        let b = a[Math.floor(Math.random()*a.length)];
+    
+    while(string.length !== 8){
+        //Math.floor() : 소수점 이하를 버림
+        //Math.cell() : 소수점 이하를 올림
+        //Math.round() : 소수점 이하를 반올림
+        let b = a[Math.floor(Math.random() * a.length)];
         if(!string.includes(b)){
             string.push(b);
         }
     }
-
     let medicine = string.join('');
     return medicine;
 }
@@ -20,9 +27,29 @@ function randomItem(a){
 
 let total_medicine = [];
 
-for(let i = 0; i< 100; i++){
+while(total_medicine.length !== 100){
     let m = randomItem(l);
-    if(!totoal_medicine.includes(m)){
+    if(!total_medicine.includes(m)){
         total_medicine.push(m);
     }
 }
+console.log(total_medicine);
+let user_input = 'ABCDEFC 4'.split(' ');
+let result = [];
+
+
+for(let i of total_medicine){
+    let setUserData = new Set(user_input[0]);
+    let setMedicine = new Set(i);
+
+    //집합의 오브젝트(교집합, 차집합....new Set에 들어있음)
+    let interSection = new Set([...setUserData].filter(x => setMedicine.has(x)));
+    // console.log(intersection);
+    if(interSection.size === parseInt(user_input[1], 10)){
+        result.push(i);
+      }
+}
+
+
+console.log(result);
+console.log(result.length);
